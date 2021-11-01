1dbf68d3bd217f0f83a1d5c6eec091224b7f24aa
Mooyeonkim628 <54655773+Mooyeonkim628@users.noreply.github.com>
2021-10-31 23:33:04 -0400

    Update lint.py

--- package_scorer/lint.py
+++ package_scorer/lint.py
@@ -1,11 +1,8 @@
-@@ -1,5 +1,8 @@
-+"""
-+pylint example
-+"""
+"""
+pylint example
+"""
  for i in range(1, 10):
      for j in range(1, 10):
--        print ("{} * {} = {}".format(i, j, i*j))
-+        print("{} * {} = {}".format(i, j, i*j))
+        print("{} * {} = {}".format(i, j, i*j))
      if j == 9:
--        print ("")
-+        print("")
+        print("")
