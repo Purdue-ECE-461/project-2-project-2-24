e21483e583c3d1f39ff1831c2756203f40f38069
Mooyeonkim628 <54655773+Mooyeonkim628@users.noreply.github.com>
2021-10-31 23:31:51 -0400

    Update lint.py

--- package_scorer/lint.py
+++ package_scorer/lint.py
@@ -1,9 +1,11 @@
-"""
-Code to add two numbers
-"""
-
-a = 23
-b = 45
-c = a + b
-
-print(c)
+@@ -1,5 +1,8 @@
++"""
++pylint example
++"""
+ for i in range(1, 10):
+     for j in range(1, 10):
+-        print ("{} * {} = {}".format(i, j, i*j))
++        print("{} * {} = {}".format(i, j, i*j))
+     if j == 9:
+-        print ("")
++        print("")
