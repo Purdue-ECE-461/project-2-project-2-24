a2d440342340f49f85b48ba3fe9b2189aef54fe7
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2021-11-09 11:41:55 -0500

    #13 #16 Stubbing out upload and helper functions

--- server/openapi_server/database/database.py
+++ server/openapi_server/database/database.py
@@ -11,19 +11,88 @@ class Database():
         self.tables = self.client.list_tables(self.dataset)
     
 
-    # Params: package: models/Package
-    def upload_package(self, package):
-        # Get package ID
-        query = """
-        INSERT INTO ece-461-proj-2-24.module_registry.packages (id, name, url, version, sensitive, secret, upload_user_id, zip)
-        VALUES (1, "sample", "https://www.sample.com", "1.0.0", false, false, 1, "base64encodedpackage")
-        """
-        query_job = self.client.query(query)  # Make an API request.
+    # Params: 
+    # user: models/User
+    # package: models/Package
+    def upload_package(self, user, package):
+        # Get metadata and data
+        metadata, data = package.metadata(), package.data()
 
-        print("The query response:")
-        print(query_job)
-        for row in query_job:
-            # Row values can be accessed by field name or index.
-            print(row)
+        # Check if package already exists
+        name = metadata.name()
+        version = metadata.version()
+        if self.package_exists(name, version):
+            # TODO: Return error saying package already exists, use package update to change existing package
+            return "some error"
+
+        # Get id
+        id = metadata.id()
+        if id is None or self.id_exists("packages", id):
+            # TODO: Include new id in response body
+            id = self.gen_new_id("packages")
+        
+        # Content or URL or both should be set for upload
+        content = data.content()
+        url = data.url()
+        if content is None and url is None:
+            # TODO: Return error saying missing content or URL
+            return "some error"
+
+        # TODO: Implement sensitive and secret flags
+        sensitive = False
+        secret = True
+        
+        # Get js_program
+        js_program = data.js_program()
+        # TODO: Trigger separate query to insert js program first, then add the ID to the package upload
+
+        # Get user id
+        upload_user_id = user.id()
+
+        # Generate query
+        query = f"""
+            INSERT INTO {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset}.packages (id, name, url, version, sensitive, secret, upload_user_id, zip)
+            VALUES ({id}, "{name}", "{url}", "{version}", {sensitive}, {secret}, {upload_user_id}, "{content}")
+        """
+
+        results = self.execute_query(query)
 
         return None
+
+
+    def gen_new_id(self, table):
+        # TODO: Find lowest available positive integer ID in given table and return it
+        return 5
+
+    
+    def id_exists(self, table, id):
+        # Generate query
+        query = f"""
+            SELECT id from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset}.{table} WHERE id = {id}
+        """
+
+        results = self.execute_query(query)
+        # TODO: Check results to see if any records are returned
+        return False
+
+    
+    def package_exists(self, name, version):
+        # Generate query
+        query = f"""
+            SELECT name, version from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset}.packages WHERE name = {name} AND version = {version}
+        """
+
+        results = self.execute_query(query)
+
+        # TODO: Check results to see if any records are returned
+        return False
+
+
+    def execute_query(self, query):
+        query_job = self.client.query(query)
+
+        print("QUERY RESPONSE:")
+        for row in query_job:
+            print(row)
+
+        return query_job
