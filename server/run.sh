5baf55eb797fac1a6c7d7e57e15c9cbc0e31be76
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2021-11-12 17:45:00 -0500

    Set up run file to properly install and configure server package for testing

---
+++ server/run.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Check for correct number of arguments
+if [ "$#" -ne 1 ]; then
+    echo "You must enter exactly 1 command line argument."
+    echo "Use the '-help' option to see options!"
+    exit 1
+fi
+
+if [ $1 == "-help" ]; then
+    echo "Use 'install' to install, 'test' to run tests, or 'clean' to clean install"
+elif [ $1 == "install" ]; then
+    python -m venv venv
+    source ./bin/activate
+    pip install --upgrade pip
+    pip install -e ./openapi_server
+    deactivate
+elif [[ $1 == "test" ]]; then
+    source ./bin/activate
+    > test_output.txt
+    coverage run -m pytest test.py >> test_output.txt
+    coverage report -m >> test_output.txt
+    python3 print_tests.py test_output.txt
+    deactivate
+elif [[ $1 == "clean" ]]; then
+    echo "Cleaning install..."
+    rm -r __pycache__
+    rm -r .pytest_cache
+    rm -r bin
+    rm -r include
+    rm -r lib
+    rm -r share
+    rm pyvenv.cfg
+else
+    echo "Invalid command line argument"
+    echo "Use the '-help' option to see options!"
+    exit 1
+fi
+
+exit 0
