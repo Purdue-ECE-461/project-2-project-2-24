548d2945f25cabed061c14e8067dae306248d582
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2021-11-25 22:52:34 -0500

    Updated server

---
+++ server/new_server/src/openapi_server/apis/default_api.py
@@ -0,0 +1,293 @@
+# coding: utf-8
+
+from typing import Dict, List  # noqa: F401
+
+from fastapi import (  # noqa: F401
+    APIRouter,
+    Body,
+    Cookie,
+    Depends,
+    Form,
+    Header,
+    Path,
+    Query,
+    Response,
+    Security,
+    status,
+)
+
+from openapi_server.models.extra_models import TokenModel  # noqa: F401
+from openapi_server.models.authentication_request import AuthenticationRequest
+from openapi_server.models.error import Error
+from openapi_server.models.package import Package
+from openapi_server.models.package_history_entry import PackageHistoryEntry
+from openapi_server.models.package_metadata import PackageMetadata
+from openapi_server.models.package_query import PackageQuery
+from openapi_server.models.package_rating import PackageRating
+from openapi_server.models.user import User
+from openapi_server.models.user_group import UserGroup
+
+
+router = APIRouter()
+db = Database()
+
+
+def token_from_auth(auth):
+    return auth.split()[-1]
+
+@router.put(
+    "/authenticate",
+    responses={
+        200: {"model": str, "description": "Success."},
+        401: {"description": "Authentication failed (e.g. no such user or invalid password)"},
+        501: {"description": "This system does not support authentication."},
+    },
+    tags=["default"],
+)
+async def create_auth_token(
+    authentication_request: AuthenticationRequest = Body(None, description=""),
+) -> str:
+    return db.create_new_token(authentication_request)
+
+
+@router.post(
+    "/usergroups",
+    responses={
+        201: {"description": "Successful response."},
+    },
+    tags=["default"],
+    summary="Create a UserGroup",
+)
+async def create_user_group(
+    x_authorization: str = Header(None, description=""),
+    user_group: UserGroup = Body(None, description="A new UserGroup to be created."),
+) -> None:
+    """Creates a new instance of a UserGroup."""
+    ...
+
+
+@router.delete(
+    "/usergroups/{usergroupId}",
+    responses={
+        204: {"description": "Successful response."},
+    },
+    tags=["default"],
+    summary="Delete a UserGroup",
+)
+async def delete_user_group(
+    usergroupId: str = Path(None, description="A unique identifier for a &#x60;UserGroup&#x60;."),
+    x_authorization: str = Header(None, description=""),
+) -> None:
+    """Deletes an existing UserGroup."""
+    ...
+
+
+@router.get(
+    "/usergroups/{usergroupId}",
+    responses={
+        200: {"model": UserGroup, "description": "Successful response - returns a single UserGroup."},
+    },
+    tags=["default"],
+    summary="Get a UserGroup",
+)
+async def get_user_group(
+    usergroupId: str = Path(None, description="A unique identifier for a UserGroup."),
+    x_authorization: str = Header(None, description=""),
+) -> UserGroup:
+    """Gets the details of a single instance of a UserGroup."""
+    ...
+
+
+@router.get(
+    "/usergroups",
+    responses={
+        200: {"model": List[UserGroup], "description": "Successful response - returns an array of UserGroup entities."},
+    },
+    tags=["default"],
+    summary="List All UserGroups",
+)
+async def get_user_groups(
+) -> List[UserGroup]:
+    """Gets a list of all UserGroup entities."""
+    ...
+
+
+@router.delete(
+    "/package/byName/{name}",
+    responses={
+        200: {"description": "Package is deleted."},
+        400: {"description": "No such package."},
+    },
+    tags=["default"],
+    summary="Delete all versions of this package.",
+)
+async def package_by_name_delete(
+    name: str = Path(None, description=""),
+    x_authorization: str = Header(None, description=""),
+) -> None:
+    ...
+
+
+@router.get(
+    "/package/byName/{name}",
+    responses={
+        200: {"model": List[PackageHistoryEntry], "description": "Package history"},
+        400: {"description": "No such package."},
+        200: {"model": Error, "description": "unexpected error"},
+    },
+    tags=["default"],
+)
+async def package_by_name_get(
+    name: str = Path(None, description=""),
+    x_authorization: str = Header(None, description=""),
+) -> List[PackageHistoryEntry]:
+    """Return the history of this package (all versions)."""
+    ...
+
+
+@router.post(
+    "/package",
+    responses={
+        201: {"model": PackageMetadata, "description": "Success. Check the ID in the returned metadata for the official ID."},
+        400: {"description": "Malformed request."},
+        403: {"description": "Package exists already."},
+    },
+    tags=["default"],
+)
+async def package_create(
+    x_authorization: str = Header(None, description=""),
+    package: Package = Body(None, description=""),
+) -> PackageMetadata:
+    metadata = db.upload_package(token_from_auth(x_authorization), package)
+    return metadata
+
+
+@router.delete(
+    "/package/{id}",
+    responses={
+        200: {"description": "Package is deleted."},
+        400: {"description": "No such package."},
+    },
+    tags=["default"],
+    summary="Delete this version of the package.",
+)
+async def package_delete(
+    id: str = Path(None, description="Package ID"),
+    x_authorization: str = Header(None, description=""),
+) -> None:
+    ...
+
+
+@router.get(
+    "/package/{id}/rate",
+    responses={
+        200: {"model": PackageRating, "description": "Rating. Only use this if each metric was computed successfully."},
+        400: {"description": "No such package."},
+        500: {"description": "The package rating system choked on at least one of the metrics."},
+    },
+    tags=["default"],
+)
+async def package_rate(
+    id: str = Path(None, description=""),
+    x_authorization: str = Header(None, description=""),
+) -> PackageRating:
+    ...
+
+
+@router.get(
+    "/package/{id}",
+    responses={
+        200: {"model": Package, "description": "pet response"},
+        200: {"model": Error, "description": "unexpected error"},
+    },
+    tags=["default"],
+)
+async def package_retrieve(
+    id: str = Path(None, description="ID of package to fetch"),
+    x_authorization: str = Header(None, description=""),
+) -> Package:
+    """Return this package."""
+    ...
+
+
+@router.put(
+    "/package/{id}",
+    responses={
+        200: {"description": "Success."},
+        400: {"description": "Malformed request (e.g. no such package)."},
+    },
+    tags=["default"],
+    summary="Update this version of the package.",
+)
+async def package_update(
+    id: str = Path(None, description=""),
+    package: Package = Body(None, description=""),
+    x_authorization: str = Header(None, description=""),
+) -> None:
+    """The name, version, and ID must match.  The package contents (from PackageData) will replace the previous contents."""
+    ...
+
+
+@router.post(
+    "/packages",
+    responses={
+        200: {"model": List[PackageMetadata], "description": "List of packages"},
+        200: {"model": Error, "description": "unexpected error"},
+    },
+    tags=["default"],
+    summary="Get packages",
+)
+async def packages_list(
+    package_query: List[PackageQuery] = Body(None, description=""),
+    x_authorization: str = Header(None, description=""),
+    offset: str = Query(None, description="Provide this for pagination. If not provided, returns the first page of results."),
+) -> List[PackageMetadata]:
+    """Get any packages fitting the query."""
+    ...
+
+
+@router.delete(
+    "/reset",
+    responses={
+        200: {"description": "Registry is reset."},
+        401: {"description": "You do not have permission to reset the registry."},
+    },
+    tags=["default"],
+)
+async def registry_reset(
+    x_authorization: str = Header(None, description=""),
+) -> None:
+    ...
+
+
+@router.put(
+    "/usergroups/{usergroupId}",
+    responses={
+        202: {"description": "Successful response."},
+    },
+    tags=["default"],
+    summary="Update a UserGroup",
+)
+async def update_user_group(
+    usergroupId: str = Path(None, description="A unique identifier for a &#x60;UserGroup&#x60;."),
+    x_authorization: str = Header(None, description=""),
+    user_group: UserGroup = Body(None, description="Updated &#x60;UserGroup&#x60; information."),
+) -> None:
+    """Updates an existing UserGroup."""
+    ...
+
+
+@router.post(
+    "/user",
+    responses={
+        200: {"model": User, "description": "User successfully created."},
+    },
+    tags=["default"],
+    summary="Create a new user",
+)
+async def user_create(
+    x_authorization: str = Header(None, description=""),
+    user: User = Body(None, description="New user to register."),
+) -> User:
+    """Create a new registered user. Pass in User in body, and AuthorizationToken in header. AuthorizationToken must belong to user with Admin privileges."""
+    ...
---
+++ server/new_server/tests/test_default_api.py
@@ -0,0 +1,332 @@
+# coding: utf-8
+
+from fastapi.testclient import TestClient
+
+
+from openapi_server.models.authentication_request import AuthenticationRequest  # noqa: F401
+from openapi_server.models.error import Error  # noqa: F401
+from openapi_server.models.package import Package  # noqa: F401
+from openapi_server.models.package_history_entry import PackageHistoryEntry  # noqa: F401
+from openapi_server.models.package_metadata import PackageMetadata  # noqa: F401
+from openapi_server.models.package_query import PackageQuery  # noqa: F401
+from openapi_server.models.package_rating import PackageRating  # noqa: F401
+from openapi_server.models.user import User  # noqa: F401
+from openapi_server.models.user_group import UserGroup  # noqa: F401
+
+
+def test_create_auth_token(client: TestClient):
+    """Test case for create_auth_token
+
+    
+    """
+    authentication_request = {"secret":{"password":"password"},"user":{"name":"Alfalfa","is_admin":1,"user_authentication_info":{"password":"password"},"user_group":{"name":"Admins","upload":1,"search":1,"download":1,"register":1}}}
+
+    headers = {
+    }
+    response = client.request(
+        "PUT",
+        "/authenticate",
+        headers=headers,
+        json=authentication_request,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_create_user_group(client: TestClient):
+    """Test case for create_user_group
+
+    Create a UserGroup
+    """
+    user_group = {"name":"Admins","upload":1,"search":1,"download":1,"register":1}
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "POST",
+        "/usergroups",
+        headers=headers,
+        json=user_group,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_delete_user_group(client: TestClient):
+    """Test case for delete_user_group
+
+    Delete a UserGroup
+    """
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "DELETE",
+        "/usergroups/{usergroupId}".format(usergroupId='usergroup_id_example'),
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_get_user_group(client: TestClient):
+    """Test case for get_user_group
+
+    Get a UserGroup
+    """
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "GET",
+        "/usergroups/{usergroupId}".format(usergroupId='usergroup_id_example'),
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_get_user_groups(client: TestClient):
+    """Test case for get_user_groups
+
+    List All UserGroups
+    """
+
+    headers = {
+    }
+    response = client.request(
+        "GET",
+        "/usergroups",
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_package_by_name_delete(client: TestClient):
+    """Test case for package_by_name_delete
+
+    Delete all versions of this package.
+    """
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "DELETE",
+        "/package/byName/{name}".format(name='name_example'),
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_package_by_name_get(client: TestClient):
+    """Test case for package_by_name_get
+
+    
+    """
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "GET",
+        "/package/byName/{name}".format(name='name_example'),
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_package_create(client: TestClient):
+    """Test case for package_create
+
+    
+    """
+    package = {"metadata":{"secret":1,"version":"1.2.3","sensitive":1,"id":"ID","name":"Name"},"data":{"content":"Content","js_program":"JSProgram","url":"URL"}}
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "POST",
+        "/package",
+        headers=headers,
+        json=package,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_package_delete(client: TestClient):
+    """Test case for package_delete
+
+    Delete this version of the package.
+    """
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "DELETE",
+        "/package/{id}".format(id='id_example'),
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_package_rate(client: TestClient):
+    """Test case for package_rate
+
+    
+    """
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "GET",
+        "/package/{id}/rate".format(id='id_example'),
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_package_retrieve(client: TestClient):
+    """Test case for package_retrieve
+
+    
+    """
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "GET",
+        "/package/{id}".format(id='id_example'),
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_package_update(client: TestClient):
+    """Test case for package_update
+
+    Update this version of the package.
+    """
+    package = {"metadata":{"secret":1,"version":"1.2.3","sensitive":1,"id":"ID","name":"Name"},"data":{"content":"Content","js_program":"JSProgram","url":"URL"}}
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "PUT",
+        "/package/{id}".format(id='id_example'),
+        headers=headers,
+        json=package,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_packages_list(client: TestClient):
+    """Test case for packages_list
+
+    Get packages
+    """
+    package_query = [{"version":"Exact (1.2.3)\nBounded range (1.2.3-2.1.0)\nCarat (^1.2.3)\nTilde (~1.2.0)","name":"Name"}]
+    params = [("offset", 'offset_example')]
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "POST",
+        "/packages",
+        headers=headers,
+        json=package_query,
+        params=params,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_registry_reset(client: TestClient):
+    """Test case for registry_reset
+
+    
+    """
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "DELETE",
+        "/reset",
+        headers=headers,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_update_user_group(client: TestClient):
+    """Test case for update_user_group
+
+    Update a UserGroup
+    """
+    user_group = {"name":"Admins","upload":1,"search":1,"download":1,"register":1}
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "PUT",
+        "/usergroups/{usergroupId}".format(usergroupId='usergroup_id_example'),
+        headers=headers,
+        json=user_group,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
+
+def test_user_create(client: TestClient):
+    """Test case for user_create
+
+    Create a new user
+    """
+    user = {"name":"Alfalfa","is_admin":1,"user_authentication_info":{"password":"password"},"user_group":{"name":"Admins","upload":1,"search":1,"download":1,"register":1}}
+
+    headers = {
+        "x_authorization": 'x_authorization_example',
+    }
+    response = client.request(
+        "POST",
+        "/user",
+        headers=headers,
+        json=user,
+    )
+
+    # uncomment below to assert the status code of the HTTP response
+    #assert response.status_code == 200
+
