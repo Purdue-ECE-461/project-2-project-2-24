b5b8eab9a9f2074d89afb8a275f81648d2b7c951
Aiden Gonzalez <aidenlgonzalez2@gmail.com>
2021-11-30 21:16:11 -0500

    #43 refactoring and upload improvements

--- server/src/openapi_server/database/database.py
+++ server/src/openapi_server/database/database.py
@@ -38,6 +38,137 @@ class Database:
             print("Cannot initialize Database!!!")
             print(err)
 
+    # ________________________________________________________________________________________________________________
+    #                                                   HISTORY
+    # ________________________________________________________________________________________________________________
+
+    # ________________________________________________________________________________________________________________
+    #                                                   PACKAGES
+    # ________________________________________________________________________________________________________________
+
+    # Params:
+    # auth: token (string)
+    # package: package (models/Package)
+    def upload_package(self, token, package):
+        # Get metadata and data
+        metadata, data = package.metadata, package.data
+
+        # Add check if package already exists
+        # TODO: * character is reserved
+        name = metadata.name
+        version = metadata.version
+        if self.package_exists(name, version):
+            return Error(code=403, message="Package-Version already exists, use 'Update' instead!")
+
+        # Get id
+        package_id = metadata.id
+        if package_id is None:
+            package_id = name + "_" + version
+        elif self.package_id_exists(package_id):
+            package_id = package_id + "_" + name + "_" + version
+        metadata.id = package_id
+
+        # Get user id
+        upload_user_id = self.get_user_id_from_token(token)
+        if upload_user_id is None:
+            return Error(code=500, message="Cannot find ID of uploading user!!")
+
+        # TODO: Implement sensitive and secret flags
+        sensitive = package.sensitive
+        secret = package.secret
+
+        if sensitive:
+            # Get js_program
+            js_program = data.js_program
+            if js_program is not None:
+                self.upload_js_program(package_id, js_program)
+
+        # Content provided for package upload, URL provided for package ingest
+        content = data.content
+        url = data.url
+        if content is None and url is not None:
+            # Ingest package query
+            query = f"""
+                                    INSERT INTO {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.packages (id, name, url, version, sensitive, secret, upload_user_id, zip)
+                                    VALUES ("{package_id}", "{name}", "{url}", "{version}", {sensitive}, {secret}, {upload_user_id}, NULL)
+                                """
+        elif content is not None and url is None:
+            # Upload package query
+            query = f"""
+                        INSERT INTO {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.packages (id, name, url, version, sensitive, secret, upload_user_id, zip)
+                        VALUES ("{package_id}", "{name}", NULL, "{version}", {sensitive}, {secret}, {upload_user_id}, "{content}")
+                    """
+        elif content is not None and url is not None:
+            return Error(code=400, message="Cannot provide both Content and URL in the same request!")
+        else:
+            # Both are not provided
+            return Error(code=400, message="Missing URL or Content!")
+
+        results = self.execute_query(query)
+
+        if isinstance(results, Error):
+            return results
+        else:
+            return metadata
+
+    def package_id_exists(self, id):
+        # Generate query
+        query = f"""
+            SELECT id from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.packages WHERE id = "{id}"
+        """
+
+        results = self.execute_query(query)
+
+        if isinstance(results, Error):
+            return results
+        else:
+            return len(results) > 0
+
+    def package_exists(self, name, version):
+        # Generate query
+        query = f"""
+            SELECT name, version from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.packages WHERE name = "{name}" AND version = "{version}"
+        """
+
+        results = self.execute_query(query)
+
+        if isinstance(results, Error):
+            return results
+        else:
+            return len(results) > 0
+
+    # ________________________________________________________________________________________________________________
+    #                                                   RATINGS
+    # ________________________________________________________________________________________________________________
+
+    # ________________________________________________________________________________________________________________
+    #                                                   SCRIPTS
+    # ________________________________________________________________________________________________________________
+
+    def upload_js_program(self, package_id, js_program):
+        # Get new script id
+        js_program_id = self.gen_new_integer_id("scripts")
+        if js_program_id is None:
+            return Error(code=500, message="Couldn't generate new js_program id!")
+
+        # Generate query
+        query = f"""
+            INSERT INTO {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.scripts (id, package_id, script)
+            VALUES ({js_program_id}, "{package_id}", "{js_program.encode("unicode_escape").decode("utf-8")}")
+        """
+
+        # Execute query
+        results = self.execute_query(query)
+
+        if isinstance(results, Error):
+            return results
+        else:
+            return js_program_id
+
+    # ________________________________________________________________________________________________________________
+    #                                                   TOKENS
+    # ________________________________________________________________________________________________________________
+
     def create_new_token(self, auth_request):
 
         user_id = self.get_user_id(auth_request.user.name)
@@ -73,89 +204,56 @@ class Database:
         else:
             return None
 
-    # Params:
-    # auth: token (string)
-    # package: package (models/Package)
-    def upload_package(self, token, package):
-        # Get metadata and data
-        metadata, data = package.metadata, package.data
+    # ________________________________________________________________________________________________________________
+    #                                                USER GROUPS
+    # ________________________________________________________________________________________________________________
 
-        # Add check if package already exists
-        # TODO: * character is reserved
-        name = metadata.name
-        version = metadata.version
-        if self.package_exists(name, version):
-            return Error(code=403, message="Package-Version already exists, use 'Update' instead!")
+    def create_new_user_group(self, token, user_group):
+        # Is user allowed to create a new user?
+        if not user.is_admin:
+            return Error(code=401, message="Must be admin to create new user")
 
-        # Get id
-        package_id = metadata.id
-        if package_id is None:
-            package_id = name + "_" + version
-        elif self.package_id_exists(package_id):
-            package_id = package_id + "_" + name + "_" + version
-        metadata.id = package_id
+        # Generate id for new user
+        new_user_id = self.gen_new_integer_id("users")
 
-        # Content or URL or both should be set for upload
-        content = data.content
-        url = data.url
-        if content is None and url is None:
-            if content is None:
-                return Error(code=400, message="Missing package contents for upload!")
-            else:
-                return Error(code=400, message="Missing URL for ingest!")
+        # Get username
+        new_user_username = new_user.name
 
-        # Get user id
-        upload_user_id = self.get_user_id_from_token(token)
-        if upload_user_id is None:
-            return Error(code=500, message="Cannot find ID of uploading user!!")
+        # Get password
+        new_user_password_hash = utils.db_hash(password)
 
-        # TODO: Implement sensitive and secret flags
-        # Do this by adding these flags to Project metadata?
-        # While you're add it, add user group to user model (in yaml spec)
-        # Maybe add UserGroup model as well?
-        # While you're add it, add any other models needed
-        # See if we're allowed to change the spec first
-        sensitive = True
-        secret = True
-
-        if sensitive:
-            # Get js_program
-            js_program = data.js_program
-            if js_program is not None:
-                self.upload_js_program(package_id, js_program)
+        # Get user group id
+        user_group_id = self.get_user_group_id(user_group)
+        if user_group_id is None:
+            return Error(code=400, message="Cannot find user group with provided name! User not created!")
 
         # Generate query
         query = f"""
-            INSERT INTO {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.packages (id, name, url, version, sensitive, secret, upload_user_id, zip)
-            VALUES ("{package_id}", "{name}", "{url}", "{version}", {sensitive}, {secret}, {upload_user_id}, "{content}")
-        """
+                    INSERT INTO {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.users (id, username, hash_pass, user_group_id)
+                    VALUES ({new_user_id}, "{new_user_username}", "{new_user_password_hash}", "{user_group_id}")
+                """
 
+        results = self.execute_query(query)
+
+        return results
+
+    def get_user_group_id(self, group_name):
+        # Generate query
+        query = f"""
+            SELECT id from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.user_groups WHERE name = "{group_name}"
+        """
         results = self.execute_query(query)
 
         if isinstance(results, Error):
             return results
+        elif len(results) == 0:
+            return None
         else:
-            return metadata
+            return results[0].get("id", default=None)
 
-    def upload_js_program(self, package_id, js_program):
-        # Get new script id
-        js_program_id = self.gen_new_integer_id("scripts")
-        if js_program_id is None:
-            return Error(code=500, message="Couldn't generate new js_program id!")
-
-        # Generate query
-        query = f"""
-            INSERT INTO {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.scripts (id, package_id, script)
-            VALUES ({js_program_id}, "{package_id}", "{js_program.encode("unicode_escape").decode("utf-8")}")
-        """
-
-        # Execute query
-        results = self.execute_query(query)
-
-        if isinstance(results, Error):
-            return results
-        else:
-            return js_program_id
+    # ________________________________________________________________________________________________________________
+    #                                                   USERS
+    # ________________________________________________________________________________________________________________
 
     def create_new_user(self, user, new_user, password, user_group):
         # Is user allowed to create a new user?
@@ -186,35 +284,24 @@ class Database:
 
         return results
 
-    def create_new_user_group(self, token, user_group):
-        # Is user allowed to create a new user?
-        if not user.is_admin:
-            return Error(code=401, message="Must be admin to create new user")
-
-        # Generate id for new user
-        new_user_id = self.gen_new_integer_id("users")
-
-        # Get username
-        new_user_username = new_user.name
-
-        # Get password
-        new_user_password_hash = utils.db_hash(password)
-
-        # Get user group id
-        user_group_id = self.get_user_group_id(user_group)
-        if user_group_id is None:
-            return Error(code=400, message="Cannot find user group with provided name! User not created!")
-
+    def get_user_id(self, name):
         # Generate query
         query = f"""
-                    INSERT INTO {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.users (id, username, hash_pass, user_group_id)
-                    VALUES ({new_user_id}, "{new_user_username}", "{new_user_password_hash}", "{user_group_id}")
-                """
+            SELECT id from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.users WHERE username = "{name}"
+        """
 
         results = self.execute_query(query)
 
-        return results
-        return
+        if isinstance(results, Error):
+            return results
+        elif len(results) == 0:
+            return None
+        else:
+            return results[0].get("id", default=None)
+
+    # ________________________________________________________________________________________________________________
+    #                                                   COMMON
+    # ________________________________________________________________________________________________________________
 
     def gen_new_integer_id(self, table):
         query = f"""
@@ -240,48 +327,6 @@ class Database:
         else:
             return results[0].get("new_id", default=None)
 
-    def get_user_group_id(self, group_name):
-        # Generate query
-        query = f"""
-            SELECT id from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.user_groups WHERE name = "{group_name}"
-        """
-        results = self.execute_query(query)
-
-        if isinstance(results, Error):
-            return results
-        elif len(results) == 0:
-            return None
-        else:
-            return results[0].get("id", default=None)
-
-    def get_user_id(self, name):
-        # Generate query
-        query = f"""
-            SELECT id from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.users WHERE username = "{name}"
-        """
-
-        results = self.execute_query(query)
-
-        if isinstance(results, Error):
-            return results
-        elif len(results) == 0:
-            return None
-        else:
-            return results[0].get("id", default=None)
-
-    def package_id_exists(self, id):
-        # Generate query
-        query = f"""
-            SELECT id from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.packages WHERE id = "{id}"
-        """
-
-        results = self.execute_query(query)
-
-        if isinstance(results, Error):
-            return results
-        else:
-            return (len(results) > 0)
-
     def gen_new_uuid(self):
         # Generate query
         query = f"""
@@ -296,19 +341,6 @@ class Database:
             return None
         else:
             return results[0].get("new_uuid")
-
-    def package_exists(self, name, version):
-        # Generate query
-        query = f"""
-            SELECT name, version from {os.environ["GOOGLE_CLOUD_PROJECT"]}.{self.dataset.dataset_id}.packages WHERE name = "{name}" AND version = "{version}"
-        """
-
-        results = self.execute_query(query)
-
-        if isinstance(results, Error):
-            return results
-        else:
-            return (len(results) > 0)
 
     def execute_query(self, query):
         print("QUERY:")
